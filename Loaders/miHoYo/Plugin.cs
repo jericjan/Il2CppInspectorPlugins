8978ca785b54ec4a2d4eb9ec2a4960c4ac362512
jericjan <38642145+jericjan@users.noreply.github.com>
2022-04-18 20:40:55 +0800

    mihoyo Plugin.cs v1

--- Loaders/miHoYo/Plugin.cs
+++ Loaders/miHoYo/Plugin.cs
@@ -1,14 +1,3 @@
-﻿/*
-    Copyright 2020-2021 Katy Coe - http://www.djkaty.com - https://github.com/djkaty
-
-    FOR EDUCATIONAL PURPOSES ONLY
-    All rights reserved.
-*/
-
-/*
- * This plugin demonstrates how to use a variety of techniques to deobfuscate an application
- */
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,11 +9,209 @@ using Il2CppInspector.PluginAPI;
 using Il2CppInspector.PluginAPI.V100;
 using System.Diagnostics;
 using System.Text;
+using Il2CppInspector.Model;
+using Il2CppInspector.Reflection;
 
 namespace Loader
 {
-    // Define your plugin class, implementing IPlugin plus interfaces for any hooks you wish to use
-    public class Plugin : IPlugin, ILoadPipeline
+//copied from MappedTypes.cs
+    public class Il2CppGlobalMetadataHeader
+    {
+        [SkipWhenReading]
+        public uint signature = Il2CppConstants.MetadataSignature;
+
+        [SkipWhenReading]
+        public int version = 24;
+
+        [ArrayLength(FixedSize = 0x28)]
+        public byte[] unk;
+
+        public int genericContainersOffset; // Il2CppGenericContainer
+        public int genericContainersCount;
+        public int nestedTypesOffset; // TypeDefinitionIndex
+        public int nestedTypesCount;
+        public int interfacesOffset; // TypeIndex
+        public int interfacesCount;
+        public int vtableMethodsOffset; // EncodedMethodIndex
+        public int vtableMethodsCount;
+        public int interfaceOffsetsOffset; // Il2CppInterfaceOffsetPair
+        public int interfaceOffsetsCount;
+        public int typeDefinitionsOffset; // Il2CppTypeDefinition
+        public int typeDefinitionsCount;
+
+        public int rgctxEntriesOffset; // Il2CppRGCTXDefinition
+        public int rgctxEntriesCount;
+
+        public int unk1;
+        public int unk2;
+        public int unk3;
+        public int unk4;
+
+        public int imagesOffset; // Il2CppImageDefinition
+        public int imagesCount;
+        public int assembliesOffset; // Il2CppAssemblyDefinition
+        public int assembliesCount;
+
+        public int fieldsOffset; // Il2CppFieldDefinition
+        public int fieldsCount;
+        public int genericParametersOffset; // Il2CppGenericParameter
+        public int genericParametersCount;
+
+        public int fieldAndParameterDefaultValueDataOffset; // uint8_t
+        public int fieldAndParameterDefaultValueDataCount;
+
+        public int fieldMarshaledSizesOffset; // Il2CppFieldMarshaledSize
+        public int fieldMarshaledSizesCount;
+        public int referencedAssembliesOffset; // int32_t
+        public int referencedAssembliesCount;
+
+        public int attributesInfoOffset; // Il2CppCustomAttributeTypeRange
+        public int attributesInfoCount;
+        public int attributeTypesOffset; // TypeIndex
+        public int attributeTypesCount;
+
+        public int unresolvedVirtualCallParameterTypesOffset; // TypeIndex
+        public int unresolvedVirtualCallParameterTypesCount;
+        public int unresolvedVirtualCallParameterRangesOffset; // Il2CppRange
+        public int unresolvedVirtualCallParameterRangesCount;
+
+        public int windowsRuntimeTypeNamesOffset; // Il2CppWindowsRuntimeTypeNamePair
+        public int windowsRuntimeTypeNamesSize;
+        public int exportedTypeDefinitionsOffset; // TypeDefinitionIndex
+        public int exportedTypeDefinitionsCount;
+
+        public int unk5;
+        public int unk6;
+
+        public int parametersOffset; // Il2CppParameterDefinition
+        public int parametersCount;
+
+        public int genericParameterConstraintsOffset; // TypeIndex
+        public int genericParameterConstraintsCount;
+
+        public int unk7;
+        public int unk8;
+
+        public int metadataUsagePairsOffset; // Il2CppMetadataUsagePair
+        public int metadataUsagePairsCount;
+
+        public int unk9;
+        public int unk10;
+        public int unk11;
+        public int unk12;
+
+        public int fieldRefsOffset; // Il2CppFieldRef
+        public int fieldRefsCount;
+
+        public int eventsOffset; // Il2CppEventDefinition
+        public int eventsCount;
+        public int propertiesOffset; // Il2CppPropertyDefinition
+        public int propertiesCount;
+        public int methodsOffset; // Il2CppMethodDefinition
+        public int methodsCount;
+
+        public int parameterDefaultValuesOffset; // Il2CppParameterDefaultValue
+        public int parameterDefaultValuesCount;
+
+        public int fieldDefaultValuesOffset; // Il2CppFieldDefaultValue
+        public int fieldDefaultValuesCount;
+
+        public int unk13;
+        public int unk14;
+        public int unk15;
+        public int unk16;
+
+        public int metadataUsageListsOffset; // Il2CppMetadataUsageList
+        public int metadataUsageListsCount;
+    }
+
+    public class Il2CppTypeDefinition
+    {
+        public int nameIndex;
+        public int namespaceIndex;
+        public int customAttributeIndex;
+        public int byvalTypeIndex;
+        public int byrefTypeIndex;
+
+        public int declaringTypeIndex;
+        public int parentIndex;
+        public int elementTypeIndex;
+
+        public int rgctxStartIndex;
+        public int rgctxCount;
+
+        public int genericContainerIndex;
+
+        public uint flags;
+
+        public int fieldStart;
+        public int propertyStart;
+        public int methodStart;
+        public int eventStart;
+        public int nestedTypesStart;
+        public int interfacesStart;
+        public int interfaceOffsetsStart;
+        public int vtableStart;
+
+        public ushort event_count;
+        public ushort method_count;
+        public ushort property_count;
+        public ushort field_count;
+        public ushort vtable_count;
+        public ushort interfaces_count;
+        public ushort interface_offsets_count;
+        public ushort nested_type_count;
+
+        public uint bitfield;
+        public uint token;
+    }
+
+    public class Il2CppMethodDefinition
+    {
+        public int returnType;
+        public int declaringType;
+        public int unk1;
+        public int nameIndex;
+        public int parameterStart;
+        public int genericContainerIndex;
+        public int customAttributeIndex;
+        public int reversePInvokeWrapperIndex;
+        public int unk2;
+        public int methodIndex;
+        public int invokerIndex;
+        public int rgctxCount;
+        public int rgctxStartIndex;
+        public ushort parameterCount;
+        public ushort flags;
+        public ushort slot;
+        public ushort iflags;
+        public uint token;
+    }
+
+    public class Il2CppFieldDefinition
+    {
+        public int customAttributeIndex;
+        public int typeIndex;
+        public int nameIndex;
+        public uint token;
+    }
+
+    public class Il2CppPropertyDefinition
+    {
+        public int customAttributeIndex;
+        public int nameIndex;
+        public int unk1;
+        public uint token;
+        public uint attrs;
+        public int unk2;
+        public int set;
+        public int get;
+    }
+
+
+
+// Define your plugin class, implementing IPlugin plus interfaces for any hooks you wish to use
+public class Plugin : IPlugin, ILoadPipeline
     {
         // Win32 API imports
         // Here we import functions from non-.NET DLLs that we need for our task
@@ -51,16 +238,17 @@ namespace Loader
         private delegate IntPtr GetStringLiteralFromIndex(byte[] bytes, uint index, ref int length);
 
         // Set the details of the plugin here
-        public string Id => "mihoyo";
-        public string Name => "miHoYo Loader";
-        public string Author => "Katy";
-        public string Version => "1.1.0";
-        public string Description => "Enables loading of games published by miHoYo";
+        public string Id => "honkai";
+        public string Name => "Honkai Loader";
+        public string Author => "Kur0";
+        public string Version => "1.0.0";
+        public string Description => "Enables loading of Honkai Impact 3 games";
 
         // Options
 
         // We'll need the file path to the corresponding UnityPlayer.dll (actually Honkai Impact 3.8 and 4.3 seem to be interchangeable)
-        private PluginOptionFilePath unityPath = new PluginOptionFilePath {
+        private PluginOptionFilePath unityPath = new PluginOptionFilePath
+        {
             Name = "unity-player-path",
             Description = "Path to selected UnityPlayer.dll version\n\n"
                         + "NOTE: UnityPlayer.dll from the PC release of the game is required even if you are inspecting a mobile version\n\n"
@@ -71,16 +259,15 @@ namespace Loader
             AllowedExtensions = new Dictionary<string, string> { ["dll"] = "DLL files" }
         };
 
-        private PluginOptionChoice<string> game = new PluginOptionChoice<string> {
+        private PluginOptionChoice<string> game = new PluginOptionChoice<string>
+        {
             Name = "game",
-            Description = "UnityPlayer.dll version to use for decryption",
+            Description = "Honkai version to use for decryption",
             Required = true,
-            Value = "honkai-impact-3.8",
-            Choices = new Dictionary<string, string> {
-                ["honkai-impact-3.8"]          = "Honkai Impact 3.8",
-                ["honkai-impact-4"]            = "Honkai Impact 4.3 - 4.5",
-                ["genshin-impact-1.1"]         = "Genshin Impact 1.1",
-                ["genshin-impact-1.2"]         = "Genshin Impact 1.2"
+            Value = "honkai-impact-5.6",
+            Choices = new Dictionary<string, string>
+            {
+                ["honkai-impact-5.6"] = "Honkai Impact 5.6"
             }
         };
 
@@ -97,11 +284,10 @@ namespace Loader
 
         // These are the offsets to various functions in each product's UnityPlayer.dll
         // You have to find these by reverse-engineering the code yourself
-        private Dictionary<string, UnityOffsets> Offsets = new Dictionary<string, UnityOffsets> {
-            ["honkai-impact-3.8"]          = new UnityOffsets { DecryptMetadata = 0x02B2A0, GetStringFromIndex = 0x031B00, GetStringLiteralFromIndex = 0x0353A0 },
-            ["honkai-impact-4"]            = new UnityOffsets { DecryptMetadata = 0x042110, GetStringFromIndex = 0x029660, GetStringLiteralFromIndex = 0x02CFA0 },
-            ["genshin-impact-1.1"]         = new UnityOffsets { DecryptMetadata = 0x1A7010, GetStringFromIndex = 0x12ECA0, GetStringLiteralFromIndex = 0x12EEE0 },
-            ["genshin-impact-1.2"]         = new UnityOffsets { DecryptMetadata = 0x1A7B60, GetStringFromIndex = 0x12F620, GetStringLiteralFromIndex = 0x12F860 }
+        private Dictionary<string, UnityOffsets> Offsets = new Dictionary<string, UnityOffsets>
+        {
+            ["honkai-impact-5.6"] = new UnityOffsets { DecryptMetadata = 0xC9C20, GetStringFromIndex = 0xC8AED0, GetStringLiteralFromIndex = 0xC8AEC0 },
+
         };
 
         // Handle to the loaded DLL
@@ -121,7 +307,8 @@ namespace Loader
 
         // This executes when the client begins to load a new IL2CPP application
         // Place initialization code here
-        public void LoadPipelineStarting(PluginLoadPipelineStartingEventInfo info) {
+        public void LoadPipelineStarting(PluginLoadPipelineStartingEventInfo info)
+        {
 
             // Try to load UnityPlayer.dll
             hModule = LoadLibrary(unityPath.Value);
@@ -135,7 +322,8 @@ namespace Loader
 
         // This executes when the client finishes loading an IL2CPP application
         // Place teardown code here
-        public void LoadPipelineEnding(List<Il2CppInspector.Il2CppInspector> packages, PluginLoadPipelineEndingEventInfo info) {
+        public void LoadPipelineEnding(List<Il2CppInspector.Il2CppInspector> packages, PluginLoadPipelineEndingEventInfo info)
+        {
 
             // Release memory lock on UnityPlayer.dll
             FreeLibrary(hModule);
@@ -144,7 +332,8 @@ namespace Loader
         // This executes as soon as the raw global-metadata.dat has been read from storage,
         // before any attempt is made to analyze its contents
         // We use it to call UnityPlayer.dll to decrypt the file in memory
-        public void PreProcessMetadata(BinaryObjectStream stream, PluginPreProcessMetadataEventInfo info) {
+        public void PreProcessMetadata(BinaryObjectStream stream, PluginPreProcessMetadataEventInfo info)
+        {
 
             // Assume that your plugin is enabled regardless of what is loading
             // Therefore, we don't want to process global-metadata.dat files that are not for us!
@@ -163,27 +352,14 @@ namespace Loader
             PluginServices.For(this).StatusUpdate("Decrypting metadata");
 
             // Create a delegate which internally is a function pointer to the DecryptMetadata function in the DLL
-            var pDecryptMetadata = (DecryptMetadata) Marshal.GetDelegateForFunctionPointer(ModuleBase + Offsets[game.Value].DecryptMetadata, typeof(DecryptMetadata));
+            var pDecryptMetadata = (DecryptMetadata)Marshal.GetDelegateForFunctionPointer(ModuleBase + Offsets[game.Value].DecryptMetadata, typeof(DecryptMetadata));
 
             // Call the delegate with the encrypted metadata byte array and length as arguments
-            var decryptedBytesUnmanaged = pDecryptMetadata(stream.ToArray(), (int) stream.Length);
+            var decryptedBytesUnmanaged = pDecryptMetadata(stream.ToArray(), (int)stream.Length);
 
             // Copy the decrypted data back from unmanaged memory to a byte array
             metadataBlob = new byte[stream.Length];
-            Marshal.Copy(decryptedBytesUnmanaged, metadataBlob, 0, (int) stream.Length);
-
-            // Genshin Impact adds some spice by encrypting 0x10 bytes for every 'step' bytes of the metadata
-            // with a simple XOR key, so we need to apply that too
-            if (game.Value.StartsWith("genshin-impact")) {
-                var key = new byte [] { 0xAD, 0x2F, 0x42, 0x30, 0x67, 0x04, 0xB0, 0x9C, 0x9D, 0x2A, 0xC0, 0xBA, 0x0E, 0xBF, 0xA5, 0x68 };
-
-                // The step is based on the file size
-                var step = (int) (stream.Length >> 14) << 6;
-
-                for (var pos = 0; pos < metadataBlob.Length; pos += step)
-                    for (var b = 0; b < 0x10; b++)
-                        metadataBlob[pos + b] ^= key[b];
-            }
+            Marshal.Copy(decryptedBytesUnmanaged, metadataBlob, 0, (int)stream.Length);
 
             // We replace the loaded global-metadata.dat with the newly decrypted version,
             // allowing Il2CppInspector to analyze it as normal
@@ -207,7 +383,8 @@ namespace Loader
 
         // This executes just as Il2CppInspector is about to read all of the .NET identifier strings (eg. type names).
         // We can use this to acquire the strings ourselves instead
-        public void GetStrings(Metadata metadata, PluginGetStringsEventInfo data) {
+        public void GetStrings(Metadata metadata, PluginGetStringsEventInfo data)
+        {
 
             // Don't do anything if this isn't for us
             if (!IsOurs)
@@ -245,7 +422,7 @@ namespace Loader
 
             // For each index, call the delegate with the decrypted metadata byte array and index as arguments
             foreach (var index in stringIndexes)
-                data.Strings.Add(index, Marshal.PtrToStringAnsi(pGetStringFromIndex(metadataBlob, (uint) index)));
+                data.Strings.Add(index, Marshal.PtrToStringAnsi(pGetStringFromIndex(metadataBlob, (uint)index)));
 
             // This tells Il2CppInspector we have handled the strings and not to attempt to read them itself
             // The strings will be copied from data.Strings to metadata.Strings automatically
@@ -254,13 +431,14 @@ namespace Loader
 
         // This executes just as Il2CppInspector is about to read all of the constant literal strings used in the application
         // We can use this to acquire the string literals ourselves instead
-        
+
         // String literals are indexed from 0-n, however we don't currently know the value of n,
         // and we won't be able to calculate it until the application binary has also been processed.
-        
+
         // Instead, we simply tell Il2CppInspector we have handled the string literals but actually do nothing,
         // and defer this task until later
-        public void GetStringLiterals(Metadata metadata, PluginGetStringLiteralsEventInfo data) {
+        public void GetStringLiterals(Metadata metadata, PluginGetStringLiteralsEventInfo data)
+        {
 
             // Don't do anything if this isn't for us
             if (!IsOurs)
@@ -279,7 +457,8 @@ namespace Loader
 
         // Therefore this is a good place to make any final changes to the data that the high level models and output modules will rely on
         // In this case, we are going to acquire all of the string literals that we deferred earlier
-        public void PostProcessPackage(Il2CppInspector.Il2CppInspector package, PluginPostProcessPackageEventInfo data) {
+        public void PostProcessPackage(Il2CppInspector.Il2CppInspector package, PluginPostProcessPackageEventInfo data)
+        {
 
             // Don't do anything if this isn't for us
             if (!IsOurs)
@@ -305,7 +484,8 @@ namespace Loader
             // For each index, call the delegate with the decrypted metadata byte array, index and a pointer as arguments
             // In this case, the function returns an array of UTF8-encoded characters,
             // and populates 'length' with the number of bytes returned
-            for (uint index = 0; index < stringLiteralCount; index++) {
+            for (uint index = 0; index < stringLiteralCount; index++)
+            {
                 var decryptedBytesUnmanaged = pGetStringLiteralFromIndex(metadataBlob, index, ref length);
                 var str = new byte[length];
                 Marshal.Copy(decryptedBytesUnmanaged, str, 0, length);
@@ -322,6 +502,46 @@ namespace Loader
             // Note this is different from IsStreamModified; changing the data in memory
             // does not automatically rewrite the stream.
             data.IsDataModified = true;
+        }
+
+        public void OptionsChanged(PluginOptionsChangedEventInfo e)
+        {
+            Console.Write("Skipping OptionsChanged\n");
+        }
+
+        public void PostProcessMetadata(Metadata metadata, PluginPostProcessMetadataEventInfo data)
+        {
+            Console.Write("Skipping PostProcessMetadata\n");
+        }
+
+        public void PreProcessImage(BinaryObjectStream stream, PluginPreProcessImageEventInfo data)
+        {
+            Console.Write("Skipping PreProcessImage\n");
+        }
+
+        public void PostProcessImage<T>(FileFormatStream<T> stream, PluginPostProcessImageEventInfo data) where T : FileFormatStream<T>
+        {
+            Console.Write("Skipping PostProcessImage\n");
+        }
+
+        public void PreProcessBinary(Il2CppBinary binary, PluginPreProcessBinaryEventInfo data)
+        {
+            Console.Write("Skipping PreProcessBinary\n");
+        }
+
+        public void PostProcessBinary(Il2CppBinary binary, PluginPostProcessBinaryEventInfo data)
+        {
+            Console.Write("Skipping PostProcessBinary\n");
+        }
+
+        public void PostProcessTypeModel(TypeModel model, PluginPostProcessTypeModelEventInfo data)
+        {
+            Console.Write("Skipping PostProcessTypeModel\n");
+        }
+
+        public void PostProcessAppModel(AppModel appModel, PluginPostProcessAppModelEventInfo data)
+        {
+            Console.Write("Skipping PostProcessAppModel\n");
         }
     }
 }
